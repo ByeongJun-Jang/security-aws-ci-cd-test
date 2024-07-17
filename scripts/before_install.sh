ed314aea02fadf1ceac78d6debbe0322a4f4704d
ByeongJun-Jang <qudwns8616@gmail.com>
2024-07-17 21:53:13 +0900

    ....

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,7 +4,7 @@ echo "BeforeInstall script started" > /home/ec2-user/before_install.log
 # Change to home directory
 cd /home/ec2-user
 
-# Check if the zip file exists
+# Check if the zip file exists and remove it if it does
 if [ -f "example.zip" ]; then
     echo "example.zip file exists before deletion" >> /home/ec2-user/before_install.log
     rm example.zip
@@ -13,9 +13,9 @@ else
 fi
 
 # Remove the existing jar file if it exists
-if [ -f "security-example-0.0.1-SNAPSHOT.jar" ]; then
+if [ -f "build/libs/security-example-0.0.1-SNAPSHOT-plain.jar" ]; then
     echo "Removing existing jar file" >> /home/ec2-user/before_install.log
-    rm security-example-0.0.1-SNAPSHOT.jar
+    rm build/libs/security-example-0.0.1-SNAPSHOT-plain.jar
 fi
 
 # Remove the existing appspec.yml file if it exists
@@ -28,6 +28,12 @@ fi
 if [ -f "scripts/start_server.sh" ]; then
     echo "Removing existing start_server.sh file" >> /home/ec2-user/before_install.log
     rm scripts/start_server.sh
+fi
+
+# Remove the existing before_install.sh file if it exists
+if [ -f "scripts/before_install.sh" ]; then
+    echo "Removing existing before_install.sh file" >> /home/ec2-user/before_install.log
+    rm scripts/before_install.sh
 fi
 
 echo "BeforeInstall script finished" >> /home/ec2-user/before_install.log
