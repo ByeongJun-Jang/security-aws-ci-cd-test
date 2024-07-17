72c826ca2a8e2c9f27c170a49f510b642103b621
ByeongJun-Jang <qudwns8616@gmail.com>
2024-07-17 19:39:27 +0900

    fix

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "BeforeInstall script started" > /home/ec2-user/app/before_install.log
+echo "BeforeInstall script started" > /home/ec2-user/before_install.log
 # Add any pre-installation steps here
-echo "BeforeInstall script finished" >> /home/ec2-user/app/before_install.log
+echo "BeforeInstall script finished" >> /home/ec2-user/before_install.log
