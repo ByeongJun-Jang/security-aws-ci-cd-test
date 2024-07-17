bc77965665b5a24aa32ce773a13c7a04aa447180
ByeongJun-Jang <qudwns8616@gmail.com>
2024-07-17 18:16:39 +0900

    fix : plz

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-cd /home/ec2-user/build/libs
+
+# Change to home directory
+cd /home/ec2-user
+
+# Extract the tar.gz file
+tar -xvzf example.tar.gz
+
+# Change to the build/libs directory
+cd build/libs
 
 echo "> 🟢 Run new spring services."
 JAR_NAME=$(ls | grep .jar)
