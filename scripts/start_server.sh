895b378fc1889832a19abb50c50a5f900272ddb9
ByeongJun-Jang <qudwns8616@gmail.com>
2024-07-17 17:25:25 +0900

    fix : jar file cant find

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,4 +2,5 @@
 cd /home/ec2-user/build/libs
 
 echo "> 🟢 Run new spring services."
-nohup java -jar your-app-name.jar > /dev/null 2>&1 &
+JAR_NAME=$(ls | grep .jar)
+nohup java -jar $JAR_NAME > /dev/null 2>&1 &
