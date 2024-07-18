b5cd6e0558b8a35b77724d7f4626b7af49b54def
ByeongJun-Jang <qudwns8616@gmail.com>
2024-07-18 18:32:00 +0900

    hard coding

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,10 +2,6 @@
 
 echo "BeforeInstall 스크립트 시작" > /home/ec2-user/before_install.log
 
-# 환경 변수 설정
-S3_BUCKET_NAME=${S3_BUCKET_NAME:-$(grep S3_BUCKET_NAME /etc/environment | cut -d '=' -f2)}
-S3_FILE_KEY=${S3_FILE_KEY:-$(grep S3_FILE_KEY /etc/environment | cut -d '=' -f2)}
-
 # 홈 디렉토리로 이동
 cd /home/ec2-user
 
@@ -15,13 +11,9 @@ if [ -f "/home/ec2-user/example.zip" ]; then
     rm /home/ec2-user/example.zip
 fi
 
-# 환경 변수 확인
-echo "S3_BUCKET_NAME: $S3_BUCKET_NAME" >> /home/ec2-user/before_install.log
-echo "S3_FILE_KEY: $S3_FILE_KEY" >> /home/ec2-user/before_install.log
-
 # S3에서 zip 파일 다운로드
 echo "S3에서 example.zip 다운로드" >> /home/ec2-user/before_install.log
-aws s3 cp s3://$S3_BUCKET_NAME/$S3_FILE_KEY /home/ec2-user/example.zip >> /home/ec2-user/before_install.log 2>&1
+aws s3 cp s3://groom-runner-bucket/example/example.zip /home/ec2-user/example.zip >> /home/ec2-user/before_install.log 2>&1
 
 # zip 파일이 존재하는지 확인
 if [ -f "/home/ec2-user/example.zip" ]; then
